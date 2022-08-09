58cd19c6d7e39e9900a69a389a642016a26438b7
shengkai <952470535@qq.com>
2022-08-09 23:08:46 +0800

    add definition of SeparateClouds and SegmentPlane

--- src/environment.cpp
+++ src/environment.cpp
@@ -49,10 +49,18 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     Lidar* lidar = new Lidar(cars, 0);
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+
+    // 1. 
     // renderRays(viewer, lidar->position, inputCloud);
-    renderPointCloud(viewer, inputCloud, "inputCloud");
+
+    // 2.
+    // renderPointCloud(viewer, inputCloud, "inputCloud");
+
     // TODO:: Create point processor
-  
+    ProcessPointClouds<pcl::PointXYZ> pointProcessor;
+    std::pair<typename pcl::PointCloud<pcl::PointXYZ>::Ptr, typename pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
 }
 
 // sets up different viewing angles in your window
