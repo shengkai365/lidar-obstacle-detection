c99e00f6b5735b321cd029511a031e006849ef54
shengkai <952470535@qq.com>
2022-08-10 23:24:02 +0800

    separates point clouds and clusters obstacles by pcl

--- src/environment.cpp
+++ src/environment.cpp
@@ -59,8 +59,20 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // TODO:: Create point processor
     ProcessPointClouds<pcl::PointXYZ> pointProcessor;
     std::pair<typename pcl::PointCloud<pcl::PointXYZ>::Ptr, typename pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
-    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+    // renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+    // renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
+    
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)};
+    for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster: cloudClusters)
+    {
+        std::cout << "cluster size " ; 
+        pointProcessor.numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
+        clusterId ++;
+    }
 }
 
 // sets up different viewing angles in your window
